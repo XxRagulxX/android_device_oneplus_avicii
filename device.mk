05574e607d344c472b7ea4182dc44ed993fc7419
XxRagulxX <donragulvijay@gmail.com>
2025-12-16 20:38:18 +0530

    avicii: Add support for OnePlus Camera

--- device.mk
+++ device.mk
@@ -101,6 +101,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera_metadata_shim \
+    libcamera2ndk_vendor \
+    libstdc++_vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_COPY_FILES += \
@@ -131,6 +133,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
+    libgrallocutils.vendor \
     libqdMetaData \
     libqdMetaData.system \
     libsdmcore \
@@ -152,6 +155,13 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+
+# Framework-oplus
+PRODUCT_PACKAGES += \
+    framework-oplus
+
+PRODUCT_BOOT_JARS += \
+    framework-oplus
 
 # Doze
 PRODUCT_PACKAGES += \
@@ -488,6 +498,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
+
+# Inherit from the OnePlus Apps makefile.
+$(call inherit-product, vendor/oneplus/apps/config.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/avicii/avicii-vendor.mk)
