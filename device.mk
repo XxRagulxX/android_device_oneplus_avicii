3cbfd09891f993a67160aab5d19cd2409ecabd9e
XxRagulxX <donragulvijay@gmail.com>
2025-12-16 20:50:04 +0530

    avicii: Disable thumbnail_block_model of libstagefright

--- device.mk
+++ device.mk
@@ -275,6 +275,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/media/video_system_specs.json:$(TARGET_COPY_OUT_VENDOR)/etc/video_system_specs.json
 
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+
 PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor
